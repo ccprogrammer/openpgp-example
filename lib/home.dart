14836e9ad369a49cc3f920c89ac33aead568e145
ccprogrammer <lubianca.samora@gmail.com>
2023-07-31 17:49:28 +0700

    up

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,8 @@
+import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
+import 'package:openpgp/openpgp.dart';
 import 'package:opensort/component/encrypt_message.dart';
-import 'package:opensort/component/generate_key.dart';
+import 'package:opensort/component/expandable_text.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,22 +12,84 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  KeyPair? keyPair;
+
+  generateKey() async {
+    var keyOptions = KeyOptions()..rsaBits = 2048;
+
+    keyPair = await OpenPGP.generate(
+        options: Options()
+          ..name = 'test'
+          ..email = 'test@test.com'
+          ..passphrase = '');
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        children: const [
-          GenerateKey(),
-          Divider(
+        children: [
+          buildgenerateKey(),
+          const Divider(
             height: 48,
             thickness: 2,
             color: Colors.black,
             indent: 16,
             endIndent: 16,
           ),
-          EncryptMessage(),
+          const EncryptMessage(),
         ],
       ),
+    );
+  }
+
+  buildgenerateKey() => Padding(
+        padding: const EdgeInsets.fromLTRB(16, 24, 16, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ElevatedButton(
+              onPressed: () => generateKey(),
+              child: const Text('Generate Key'),
+            ),
+            buildKeyContainer(
+              title: 'Private Key',
+              key: '${keyPair?.privateKey}',
+              onTap: () => FlutterClipboard.copy(keyPair!.privateKey),
+            ),
+            buildKeyContainer(
+              title: 'Public Key',
+              key: '${keyPair?.publicKey}',
+              onTap: () => FlutterClipboard.copy(keyPair!.publicKey),
+            ),
+          ],
+        ),
+      );
+
+  buildKeyContainer(
+      {required String title, required String key, required Function() onTap}) {
+    return Column(
+      children: [
+        const SizedBox(height: 16),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            IconButton(
+              onPressed: () => onTap(),
+              icon: const Icon(Icons.copy),
+            ),
+          ],
+        ),
+        ExpandableText(text: key, trimLines: 6),
+      ],
     );
   }
 }
